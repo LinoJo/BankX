c4bd261eaf08aa580b3f43e6f970d3d5d0ef38b8
Dennis Nüßing <dennis.nuessing@live.de>
2017-11-25 13:06:45 +0100

    erstes App Design

--- de.bankx.client.android/BankX/app/src/main/java/de/bankx/client/android/bankx/MainActivity.java
+++ de.bankx.client.android/BankX/app/src/main/java/de/bankx/client/android/bankx/MainActivity.java
@@ -1,7 +1,11 @@
 package de.bankx.client.android.bankx;
 
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void loginServer(View view){
+
+        EditText ipAdresse = (EditText) findViewById(R.id.eingabeIP);
+        EditText kontonummer = (EditText) findViewById(R.id.eingabeKontonummer);
+
+        String ip = ipAdresse.getText().toString();
+
+        kontonummer.setText(String.valueOf(ip));
+    }
+
 }
