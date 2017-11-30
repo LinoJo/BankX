6b500fd6be60b6266038a1043b3b82254a0441e2
Linus Jolmes <l.jolmes@gmx.de>
2017-11-30 02:39:22 +0100

    JavaFX Client GUI & Controller v0.9

--- de.bankx.client.javafx/src/application/BankX_Client_Controller.java
+++ de.bankx.client.javafx/src/application/BankX_Client_Controller.java
@@ -2,29 +2,21 @@ package application;
 
 import java.io.IOException;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 
 public class BankX_Client_Controller {
 
-    @FXML
-    private Button btnVerbindung;
+	@FXML
+    private TabPane tabPane;
 
     @FXML
-    private Label lblLogo;
-
-    @FXML
-    private Label lblWarnung;
+    private Tab tabLogin;
 
     @FXML
     private TextField txtKontonummer;
@@ -33,18 +25,74 @@ public class BankX_Client_Controller {
     private TextField txtServerip;
 
     @FXML
-    void handleButtonAction(ActionEvent event) {
-    	String Kontonummer = txtKontonummer.getText().toString();
-    	String Serverip = txtServerip.getText().toString();
+    private Label lblWarnungLogin;
 
+    @FXML
+    private Button btnVerbindung;
 
-    	if(event.getSource().equals(btnVerbindung)){
+    @FXML
+    private Tab tabIndex;
 
-    		// doConnection
+    @FXML
+    private Label lblKontoinhaber;
 
-    		//Warning
-    		//lblWarnung.setText("Warnung!");
+    @FXML
+    private Label lblKontonummer;
 
+    @FXML
+    private Label lblKontostand;
+
+    @FXML
+    private Button btnPageTransaktionshistorie;
+
+    @FXML
+    private Button btnPageUeberweisung;
+
+    @FXML
+    private Tab tabUeberweisung;
+
+    @FXML
+    private TextField txtEmpfaenger;
+
+    @FXML
+    private TextField txtBetrag;
+
+    @FXML
+    private Button btnSenden;
+
+    @FXML
+    private Tab tabTransaktionshistorie;
+
+    @FXML
+    private Button btnAbmelden;
+
+    @FXML
+    void handleButtonEvent(ActionEvent event) {
+    	if(event.getSource().equals(btnVerbindung))
+    	{	// TODO : handelConnection
+    		//txtKontonummer.getText();
+    		//txtServerip.getText();
+
+    	}
+    	else if(event.getSource().equals(btnAbmelden))
+    	{
+    		// TODO : close connection
+    		tabPane.getSelectionModel().select(tabIndex);
+    	}
+    	else if(event.getSource().equals(btnPageTransaktionshistorie))
+    	{
+    		tabPane.getSelectionModel().select(tabTransaktionshistorie);
+    	}
+    	else if(event.getSource().equals(btnPageUeberweisung))
+    	{
+    		tabPane.getSelectionModel().select(tabUeberweisung);
+    	}
+    	else if(event.getSource().equals(btnSenden))
+    	{
+    		//TODO : Send transaction
+    		//txtKontonummer.getText();
+    		//txtEmpfaenger.getText();
+    		//txtBetrag.getText();
     	}
     }
 }
